d071d5235aab110f602eebc6964d1adf25a9febc
emzeka <slipetsdenis@gmail.com>
2014-08-21 17:14:14 +0300

    add db

--- wp-content/themes/wow/sidebar.php
+++ wp-content/themes/wow/sidebar.php
@@ -1,123 +1,123 @@
 <div class="aside">
-            <h2 class="h2-title">Реклама</h2>
-            <div class="promo">
-                <a href=""><img src="image/promo-pic.jpg" alt=""></a>
-            </div>
-            <h2 class="h2-title">Обсуждаемое</h2>
-            <div class="popular">
-                <div class="tabbable right-tabs">
-                  <ul class="nav nav-tabs right-tabs-list">
-                    <li class="active"><a href="#tab1" data-toggle="tab">ГАЙДЫ</a></li>
-                    <li><a href="#tab2" data-toggle="tab">НОВОСТИ</a></li>
-                    <li><a href="#tab3" data-toggle="tab">АДДОНЫ</a></li>
-                    <li><a href="#tab4" data-toggle="tab">ПРОФФЕСИИ</a></li>
-                  </ul>
-                  <div class="tab-content">
-                    <div class="tab-pane active" id="tab1">
-                        <ul class="right-nav-list">
-                            <li>
-                                <div class="list-pic">
-                                    <a href=""><img src="image/right-list-pic.png" alt=""></a>
-                                </div>
-                                <div class="list-text">
-                                    <a class="right-guide-title" href="">Воин танк в пвп 3.3.5a</a>
-                                    <p>Воины являются бойцами ближнего боя, натренированными в искусстве владения оружием. На поле боя </p>
-                                    <p><a href="">12 ИЮЛЯ</a> <b>•</b> <a href="">32 КОММЕНТАРИЯ</a></p>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="list-pic">
-                                    <a href=""><img src="image/right-list-pic.png" alt=""></a>
-                                </div>
-                                <div class="list-text">
-                                    <a class="right-guide-title" href="">Воин танк в пвп 3.3.5a</a>
-                                    <p>Воины являются бойцами ближнего боя, натренированными в искусстве владения оружием. На поле боя </p>
-                                    <p><a href="">12 ИЮЛЯ</a> <b>•</b> <a href="">32 КОММЕНТАРИЯ</a></p>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="list-pic">
-                                    <a href=""><img src="image/right-list-pic.png" alt=""></a>
-                                </div>
-                                <div class="list-text">
-                                    <a class="right-guide-title" href="">Воин танк в пвп 3.3.5a</a>
-                                    <p>Воины являются бойцами ближнего боя, натренированными в искусстве владения оружием. На поле боя </p>
-                                    <p><a href="">12 ИЮЛЯ</a> <b>•</b> <a href="">32 КОММЕНТАРИЯ</a></p>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="list-pic">
-                                    <a href=""><img src="image/right-list-pic.png" alt=""></a>
-                                </div>
-                                <div class="list-text">
-                                    <a class="right-guide-title" href="">Воин танк в пвп 3.3.5a</a>
-                                    <p>Воины являются бойцами ближнего боя, натренированными в искусстве владения оружием. На поле боя </p>
-                                    <p><a href="">12 ИЮЛЯ</a> <b>•</b> <a href="">32 КОММЕНТАРИЯ</a></p>
-                                </div>
-                            </li>
-                            <li>
-                                <div class="list-pic">
-                                    <a href=""><img src="image/right-list-pic.png" alt=""></a>
-                                </div>
-                                <div class="list-text">
-                                    <a class="right-guide-title" href="">Воин танк в пвп 3.3.5a</a>
-                                    <p>Воины являются бойцами ближнего боя, натренированными в искусстве владения оружием. На поле боя </p>
-                                    <p><a href="">12 ИЮЛЯ</a> <b>•</b> <a href="">32 КОММЕНТАРИЯ</a></p>
-                                </div>
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="tab-pane" id="tab2">
-                      <p>Привет, я 2-я секция.</p>
-                    </div>
-                    <div class="tab-pane" id="tab3">
-                      <p>Привет, я 3-я секция.</p>
-                    </div>
-                    <div class="tab-pane" id="tab4">
-                      <p>Привет, я 4-я секция.</p>
-                    </div>
-                  </div>
+    <h2 class="h2-title">Р РµРєР»Р°РјР°</h2>
+    <div class="promo">
+        <a href=""><img src="image/promo-pic.jpg" alt=""></a>
+    </div>
+    <h2 class="h2-title">РћР±СЃСѓР¶РґР°РµРјРѕРµ</h2>
+    <div class="popular">
+        <div class="tabbable right-tabs">
+            <ul class="nav nav-tabs right-tabs-list">
+                <li class="active"><a href="#tab1" data-toggle="tab">Р“РђР™Р”Р«</a></li>
+                <li><a href="#tab2" data-toggle="tab">РќРћР’РћРЎРўР</a></li>
+                <li><a href="#tab3" data-toggle="tab">РђР”Р”РћРќР«</a></li>
+                <li><a href="#tab4" data-toggle="tab">РџР РћР¤Р¤Р•РЎРР</a></li>
+            </ul>
+            <div class="tab-content">
+                <div class="tab-pane active" id="tab1">
+                    <ul class="right-nav-list">
+                        <li>
+                            <div class="list-pic">
+                                <a href=""><img src="image/right-list-pic.png" alt=""></a>
+                            </div>
+                            <div class="list-text">
+                                <a class="right-guide-title" href="">Р’РѕРёРЅ С‚Р°РЅРє РІ РїРІРї 3.3.5a</a>
+                                <p>Р’РѕРёРЅС‹ СЏРІР»СЏСЋС‚СЃСЏ Р±РѕР№С†Р°РјРё Р±Р»РёР¶РЅРµРіРѕ Р±РѕСЏ, РЅР°С‚СЂРµРЅРёСЂРѕРІР°РЅРЅС‹РјРё РІ РёСЃРєСѓСЃСЃС‚РІРµ РІР»Р°РґРµРЅРёСЏ РѕСЂСѓР¶РёРµРј. РќР° РїРѕР»Рµ Р±РѕСЏ </p>
+                                <p><a href="">12 РР®Р›РЇ</a> <b>в€™</b> <a href="">32 РљРћРњРњР•РќРўРђР РРЇ</a></p>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="list-pic">
+                                <a href=""><img src="image/right-list-pic.png" alt=""></a>
+                            </div>
+                            <div class="list-text">
+                                <a class="right-guide-title" href="">Р’РѕРёРЅ С‚Р°РЅРє РІ РїРІРї 3.3.5a</a>
+                                <p>Р’РѕРёРЅС‹ СЏРІР»СЏСЋС‚СЃСЏ Р±РѕР№С†Р°РјРё Р±Р»РёР¶РЅРµРіРѕ Р±РѕСЏ, РЅР°С‚СЂРµРЅРёСЂРѕРІР°РЅРЅС‹РјРё РІ РёСЃРєСѓСЃСЃС‚РІРµ РІР»Р°РґРµРЅРёСЏ РѕСЂСѓР¶РёРµРј. РќР° РїРѕР»Рµ Р±РѕСЏ </p>
+                                <p><a href="">12 РР®Р›РЇ</a> <b>в€™</b> <a href="">32 РљРћРњРњР•РќРўРђР РРЇ</a></p>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="list-pic">
+                                <a href=""><img src="image/right-list-pic.png" alt=""></a>
+                            </div>
+                            <div class="list-text">
+                                <a class="right-guide-title" href="">Р’РѕРёРЅ С‚Р°РЅРє РІ РїРІРї 3.3.5a</a>
+                                <p>Р’РѕРёРЅС‹ СЏРІР»СЏСЋС‚СЃСЏ Р±РѕР№С†Р°РјРё Р±Р»РёР¶РЅРµРіРѕ Р±РѕСЏ, РЅР°С‚СЂРµРЅРёСЂРѕРІР°РЅРЅС‹РјРё РІ РёСЃРєСѓСЃСЃС‚РІРµ РІР»Р°РґРµРЅРёСЏ РѕСЂСѓР¶РёРµРј. РќР° РїРѕР»Рµ Р±РѕСЏ </p>
+                                <p><a href="">12 РР®Р›РЇ</a> <b>в€™</b> <a href="">32 РљРћРњРњР•РќРўРђР РРЇ</a></p>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="list-pic">
+                                <a href=""><img src="image/right-list-pic.png" alt=""></a>
+                            </div>
+                            <div class="list-text">
+                                <a class="right-guide-title" href="">Р’РѕРёРЅ С‚Р°РЅРє РІ РїРІРї 3.3.5a</a>
+                                <p>Р’РѕРёРЅС‹ СЏРІР»СЏСЋС‚СЃСЏ Р±РѕР№С†Р°РјРё Р±Р»РёР¶РЅРµРіРѕ Р±РѕСЏ, РЅР°С‚СЂРµРЅРёСЂРѕРІР°РЅРЅС‹РјРё РІ РёСЃРєСѓСЃСЃС‚РІРµ РІР»Р°РґРµРЅРёСЏ РѕСЂСѓР¶РёРµРј. РќР° РїРѕР»Рµ Р±РѕСЏ </p>
+                                <p><a href="">12 РР®Р›РЇ</a> <b>в€™</b> <a href="">32 РљРћРњРњР•РќРўРђР РРЇ</a></p>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="list-pic">
+                                <a href=""><img src="image/right-list-pic.png" alt=""></a>
+                            </div>
+                            <div class="list-text">
+                                <a class="right-guide-title" href="">Р’РѕРёРЅ С‚Р°РЅРє РІ РїРІРї 3.3.5a</a>
+                                <p>Р’РѕРёРЅС‹ СЏРІР»СЏСЋС‚СЃСЏ Р±РѕР№С†Р°РјРё Р±Р»РёР¶РЅРµРіРѕ Р±РѕСЏ, РЅР°С‚СЂРµРЅРёСЂРѕРІР°РЅРЅС‹РјРё РІ РёСЃРєСѓСЃСЃС‚РІРµ РІР»Р°РґРµРЅРёСЏ РѕСЂСѓР¶РёРµРј. РќР° РїРѕР»Рµ Р±РѕСЏ </p>
+                                <p><a href="">12 РР®Р›РЇ</a> <b>в€™</b> <a href="">32 РљРћРњРњР•РќРўРђР РРЇ</a></p>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <div class="tab-pane" id="tab2">
+                    <p>РџСЂРёРІРµС‚, СЏ 2-СЏ СЃРµРєС†РёСЏ.</p>
+                </div>
+                <div class="tab-pane" id="tab3">
+                    <p>РџСЂРёРІРµС‚, СЏ 3-СЏ СЃРµРєС†РёСЏ.</p>
+                </div>
+                <div class="tab-pane" id="tab4">
+                    <p>РџСЂРёРІРµС‚, СЏ 4-СЏ СЃРµРєС†РёСЏ.</p>
                 </div>
             </div>
-            <h2 class="h2-title">КАТЕГОРИИ</h2>
-            <div class="popular">
-                <ul class="right-nav-list categories">
-                    <li>
-                        <a href="">Гайды</a>
-                        <b><a href="">12</a></b>
-                    </li>
-                    <li>
-                        <a href="">Гайды</a>
-                        <b><a href="">12</a></b>
-                    </li>
-                    <li>
-                        <a href="">Гайды</a>
-                        <b><a href="">12</a></b>
-                    </li>
-                    <li>
-                        <a href="">Гайды</a>
-                        <b><a href="">12</a></b>
-                    </li>
-                    <li>
-                        <a href="">Гайды</a>
-                        <b><a href="">12</a></b>
-                    </li>
-                    <li>
-                        <a href="">Гайды</a>
-                        <b><a href="">12</a></b>
-                    </li>
-                    <li>
-                        <a href="">Гайды</a>
-                        <b><a href="">12</a></b>
-                    </li>
-                    <li>
-                        <a href="">Гайды</a>
-                        <b><a href="">12</a></b>
-                    </li>
-                </ul>
-            </div>
-            <h2 class="h2-title">Реклама</h2>
-            <div class="promo">
-                <a href=""><img src="image/promo-pic.jpg" alt=""></a>
-            </div>
         </div>
+    </div>
+    <h2 class="h2-title">РљРђРўР•Р“РћР РР</h2>
+    <div class="popular">
+        <ul class="right-nav-list categories">
+            <li>
+                <a href="">Р“Р°Р№РґС‹</a>
+                <b><a href="">12</a></b>
+            </li>
+            <li>
+                <a href="">Р“Р°Р№РґС‹</a>
+                <b><a href="">12</a></b>
+            </li>
+            <li>
+                <a href="">Р“Р°Р№РґС‹</a>
+                <b><a href="">12</a></b>
+            </li>
+            <li>
+                <a href="">Р“Р°Р№РґС‹</a>
+                <b><a href="">12</a></b>
+            </li>
+            <li>
+                <a href="">Р“Р°Р№РґС‹</a>
+                <b><a href="">12</a></b>
+            </li>
+            <li>
+                <a href="">Р“Р°Р№РґС‹</a>
+                <b><a href="">12</a></b>
+            </li>
+            <li>
+                <a href="">Р“Р°Р№РґС‹</a>
+                <b><a href="">12</a></b>
+            </li>
+            <li>
+                <a href="">Р“Р°Р№РґС‹</a>
+                <b><a href="">12</a></b>
+            </li>
+        </ul>
+    </div>
+    <h2 class="h2-title">Р РµРєР»Р°РјР°</h2>
+    <div class="promo">
+        <a href=""><img src="image/promo-pic.jpg" alt=""></a>
+    </div>
+</div>
